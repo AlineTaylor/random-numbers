3e66996c63b1bb561bfbd32e3d083f5af2552a2d
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-01-21 21:02:44 -0600

    Created a working function that passes an object! It still doesn't communiate wih the rest of the page at all, but progress is progress lol

--- script.js
+++ script.js
@@ -1,34 +1,50 @@
 // drafting generator and response commands for level easy first
-let easyNum = Math.random()*10;
-// TODO generate script to generate numbers according to each level
+let easyNum = Math.random() * 10;
+// TODO generate function to generate numbers according to each level
+const level = {
+  easy: 5,
+  medium: 10,
+  hard: 100,
+  impossible: 1000,
+  ridiculous: 10000000,
+};
+
+function generateNumber(nivel) {
+  let displayNumber = Math.random() * nivel;
+  console.log(Math.floor(displayNumber));
+}
+
+generateNumber(level.medium);
+
 // TODO create HTML display for randomly generated number that stays hidden until guess is submitted
 const inputEasy = document.getElementById("inputEasy");
 //TODO double-check to see if ther isn't a more efficient way to code the conditions below
-if (inputEasy === easyNum){
+if (inputEasy === easyNum) {
   // Select the element with the class "correct"
-  const correctAnswer = document.querySelector('.correct');
+  const correctAnswer = document.querySelector(".correct");
   // Unhide it by changing its display property
-  correctAnswer.style.display = 'block';
-} else if (inputEasy === easyNum+1){
-    const closeAnswer = document.querySelector('.close');
-    closeAnswer.style.display = 'block';
-} else if (inputEasy === easyNum-1){
-    const closeAnswer = document.querySelector('.close');
-    closeAnswer.style.display = 'block';
-} else (inputEasy > easyNum+1){
-    const incorrectAnswer = document.querySelector('.close');
-    incorrectAnswer.style.display = 'block';}
+  correctAnswer.style.display = "block";
+} else if (inputEasy === easyNum + 1) {
+  const closeAnswer = document.querySelector(".close");
+  closeAnswer.style.display = "block";
+} else if (inputEasy === easyNum - 1) {
+  const closeAnswer = document.querySelector(".close");
+  closeAnswer.style.display = "block";
+}
+// } else (inputEasy > (easyNum+1)){
+//     const incorrectAnswer = document.querySelector('.close');
+//     incorrectAnswer.style.display = 'block';}
 
 //Try again button display
 //TODO Make this button work and isolate response by level (so not all levels are displaying the section when one is attempted)
 // Select the button and the message div
-const submitButton = document.querySelector('.submit');
-const tryAgain = document.querySelector('.tryAgain');
+const submitButton = document.querySelector(".submit");
+const tryAgain = document.querySelector(".tryAgain");
 
 // Add a click event listener to the button
-submitButton.addEventListener('click', () => {
-    // Show the hidden portion by changing its display property
-    tryAgain.style.display = 'block';
+submitButton.addEventListener("click", () => {
+  // Show the hidden portion by changing its display property
+  tryAgain.style.display = "block";
 });
 
 //TODO create reset fucntion using the tryAgain button to clear inputs but from that level only
