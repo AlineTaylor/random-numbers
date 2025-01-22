16b0aa842af73424a9a93db565cbd279282f69bf
Aline Yui Taylor <alineyui.taylor@gmail.com>
2025-01-21 21:17:22 -0600

    Connected function to evaluation if-statements. Struggling to return the function's reult - or to get it out of the fucntion...

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
-// drafting generator and response commands for level easy first
-let easyNum = Math.random() * 10;
-// TODO generate function to generate numbers according to each level
+//function that generate numbers according to each level
 const level = {
   easy: 5,
   medium: 10,
   hard: 100,
   impossible: 1000,
-  ridiculous: 10000000,
+  ridiculous: 1000000,
 };
 
-function generateNumber(nivel) {
-  let displayNumber = Math.random() * nivel;
-  console.log(Math.floor(displayNumber));
+function generateNumber(levelSelection) {
+  let displayNumber = Math.floor(Math.random() * levelSelection + 1);
+  console.log(displayNumber);
+  return displayNumber;
 }
 
-generateNumber(level.medium);
+generateNumber(level.easy); //OMG it works, holy cow, it works
+// Now what lol
 
 // TODO create HTML display for randomly generated number that stays hidden until guess is submitted
 const inputEasy = document.getElementById("inputEasy");
-//TODO double-check to see if ther isn't a more efficient way to code the conditions below
-if (inputEasy === easyNum) {
+//TODO double-check to see if there isn't a more efficient way to code the conditions below
+if (inputEasy === displayNumber) {
   // Select the element with the class "correct"
   const correctAnswer = document.querySelector(".correct");
   // Unhide it by changing its display property
@@ -30,10 +30,10 @@ if (inputEasy === easyNum) {
 } else if (inputEasy === easyNum - 1) {
   const closeAnswer = document.querySelector(".close");
   closeAnswer.style.display = "block";
+} else {
+  const incorrectAnswer = document.querySelector(".close");
+  incorrectAnswer.style.display = "block";
 }
-// } else (inputEasy > (easyNum+1)){
-//     const incorrectAnswer = document.querySelector('.close');
-//     incorrectAnswer.style.display = 'block';}
 
 //Try again button display
 //TODO Make this button work and isolate response by level (so not all levels are displaying the section when one is attempted)
